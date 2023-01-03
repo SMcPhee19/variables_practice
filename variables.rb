ae8ad006eda7092e738e82898d3962d16e94980e
Stephen McPhee <stephen.t.mcphee@gmail.com>
2023-01-03 13:17:32 -0700

    New Variables

--- variables.rb
+++ variables.rb
@@ -1,5 +1,6 @@
 name = "Stephen McPhee"
 degree = "biology"
+degree_minor = "japanese"
 hobby = "golf"
 age = 27
 birth_year = 1995
@@ -10,3 +11,13 @@ tau = 6.28
 college_graduate = true
 ate_breakfast = false
 went_to_class = true
+
+degree_minor = "chemistry"
+age = 21
+pi = 4.31
+ate_breakfast = true
+
+puts degree_minor
+puts age
+puts pi
+puts ate_breakfast
